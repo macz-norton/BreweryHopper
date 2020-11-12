d3c436aab436f2df20468a9188f694ce1caaadd3
Danny Maier <dannymaier@gmail.com>
2020-11-11 21:15:05 -0800

    added some framework for an ajax request

--- Assets/script.js
+++ Assets/script.js
@@ -3,3 +3,22 @@ var indoorOutdoorEl = $(".indoorOutdoor")
     console.log(indoorOutdoorEl)
 var zipCodeEl = $(".classZip")
     console.log(zipCodeEl)
+
+
+    //GET growstuff 
+function getPlants() {
+
+    var callType="plantings"
+    // var apiKeyMNorton = "GBqEm499B1LqBOuWTqrc3Uu4SSONPL2c5C9UU_fm_VY"
+    var queryURL="https://growstuff.org/api/v1/"+callType
+        console.log(queryURL)
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+    // .then(function(response) {
+    //     console.log(response)
+    // });
+}
+getPlants();

a7f339b46af70530b21cfeae728a07e528d053c6
Danny Maier <dannymaier@gmail.com>
2020-11-14 11:27:18 -0800

    got the for loop to add brewery information as objects to the breweries array

--- Assets/script.js
+++ Assets/script.js
@@ -21,20 +21,22 @@ function getBreweries() {
     })
     .then(function(response) {
         console.log(response)
-    });
 
-    //LOOP through the results to store each brewery as an object in our `breweriesArray`
-    for (let i = 0; i < 20; i++) {
-        var element = response[i];
-        
-    }
-
-    console.log(response.name)
-    // name
-    // street
-    // phone
-    // website
-
+    // LOOP through the results to store each brewery as an object in our `breweriesArray`
+    for (let i = 0; i < 10; i++) {
+        var breweryObj={
+            breweryName: response[i].name,
+            breweryStreet: response[i].street,
+            breweryPhone:response[i].phone,
+            breweryWebsite:response[i].website_url,
+            breweryType:response[i].brewery_type,
+            breweryLat:response[i].latitude,
+            breweryLon:response[i].longitude    
+        }
+        // console.log(breweryObj)
+        breweriesArray.push(breweryObj)
+            console.log(breweriesArray)
+    };
+});
 }
-
 getBreweries();

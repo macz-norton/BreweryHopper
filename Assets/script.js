01b8e9c536078191983bcdfbb27e5faa76aabc01
Danny Maier <dannymaier@gmail.com>
2020-11-15 09:43:17 -0800

    added dom elements for the main card

--- Assets/script.js
+++ Assets/script.js
@@ -4,11 +4,23 @@
 var cityInputEl = $("#cityName")
     console.log(cityInputEl)
 
+//Elements of the main card
+var mainCardNameEl = $(".mainCardName")
+    // console.log(mainCardNameEl)
+var mainCardTypeEl = $(".mainCardType")
+    // console.log(mainCardTypeEl)
+var mainCardAddressEl = $(".mainCardAddress")
+    // console.log(mainCardAddressEl)
+var mainCardPhoneEl = $(".mainCardPhone")
+    // console.log(mainCardPhoneEl) 
+var mainCardWebsiteEl = $(".mainCardWebsite")
+
 var searchButtonEl = $(".searchButton")
     console.log(searchButtonEl)
-var breweryCollectionEl = $(".collection")
-    console.log(breweryCollectionEl)  
-
+// var breweryCollectionEl = $(".collection")
+//     console.log(breweryCollectionEl)  
+var breweryCollectionEl = $("#resultsList")
+    console.log(breweryCollectionEl)
 
 
 //DECLARE global variables
@@ -19,6 +31,7 @@ var breweryObj;
 
 //GET breweryStuff 
 function getBreweries(boop) {
+    $(breweryCollectionEl).empty();
     var queryURL="https://api.openbrewerydb.org/breweries?by_city="+boop
         console.log(queryURL)
     //AJAX call
@@ -29,7 +42,7 @@ function getBreweries(boop) {
     .then(function(response) {
         console.log(response)
 
-    // LOOP through the results to store each brewery as an object in our `breweriesArray`
+// LOOP through the results to store each brewery as an object in our `breweriesArray`
     for (let i = 0; i < 10; i++) {
         breweryObj={
             breweryName: response[i].name,
@@ -43,27 +56,30 @@ function getBreweries(boop) {
         }
         breweriesArray.push(breweryObj)
     };
+    $(breweryCollectionEl).empty();
+//RENDER the `breweriesArray` to the `breweryCollectionEl` element
+    for (let i = 0; i < breweriesArray.length; i++) {
+        var brewery = breweriesArray[i].breweryName;
+            // console.log(breweriesArray)
+    $(breweryCollectionEl).append($("<p>").text(brewery).addClass("collection-item"));
+    }
 
-            for (let i = 0; i < breweriesArray.length; i++) {
-                var brewery = breweriesArray[i].breweryName;
-                    console.log(breweriesArray)
-                $(breweryCollectionEl).append($("<p>").text(brewery).addClass("collection-item"));
-            
-            }
-
-
-
-            
 
 });
 }
-getBreweries();
-console.log(breweriesArray)
+
+// getBreweries();
+// console.log(breweriesArray)
 
 //CLICK FUNCTIONALITY
 searchButtonEl.on("click", function(event) {
+        console.log(breweryCollectionEl)
+    breweryCollectionEl.empty();
+        console.log(breweryCollectionEl)
     event.preventDefault();
-    console.log(this);
+        console.log(this);
+    
+        console.log(breweryCollectionEl);
     
     var cityCapitalized = cityInputEl.val().trim();
         console.log(cityCapitalized)

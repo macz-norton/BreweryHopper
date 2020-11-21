0e832071b1876714e730bfbf6ed62ea68a938a77
Danny Maier <dannymaier@gmail.com>
2020-11-21 09:43:31 -0800

    tried repathing again, giving up until after presentation

--- Assets/script.js
+++ Assets/script.js
@@ -170,14 +170,16 @@ function renderBreweryCollection(){
     $(breweryCollectionEl).append($("<p>").text(brewery).addClass("collection-item"));
     }
 }
+// beerIconPath = "./Images/beerIcon.png"
 
 function myScript(){
     mainCardImg.empty();
 //the below is breaking when live on the web
     // mainCardImg.append(($("<img>")).attr("src", "https://www.freefavicon.com/freefavicons/food/beer-stein-152-202218.png"));
 //instead of the line above, i saved the image locally just to try.
-    // mainCardImg.append(($("<img>")).attr("src", "../LatePlanter/Assets/Images/beerIcon.png"));
-    mainCardImg.append(($("<img>")).attr("src", "../Assets/Images/beerIcon.png"));
+    mainCardImg.append(($("<img>")).attr("src", "../LatePlanter/Assets/Images/beerIcon.png"));
+    // mainCardImg.append(($("<img>")).attr("src", beerIconPath));
+    // mainCardImg.append(($("<img>")).attr("src", "../Assets/Images/beerIcon.png"));
     mainCardImg.append($("<p>").text("Error! Someone design this brewery a logo"))
 };
 //DECLARE the function to RENDER the main card
